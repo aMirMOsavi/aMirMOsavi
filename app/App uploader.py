14e052ba9df211312888345322d441380f62b602
aMir MOsavi <senjed_58@yahoo.com>
2023-06-07 19:30:50 +0330

    Add files via upload

---
+++ app/App uploader.py
@@ -0,0 +1,81 @@
+import requests
+from github import Github
+from datetime import datetime
+import base64
+import shutil
+import re
+import os
+
+access_token = ''
+repository_name = 'aMirMOsavi'
+
+
+# کپی کردن فایل
+shutil.copy2('./uploader.py', './uploader_copy.py')
+
+print("Removing Access_token In Python File...")
+# ویرایش فایل کپی شده
+with open('uploader_copy.py', 'r') as file:
+    content = file.read()
+# حذف مقدار access_token
+content = re.sub(r"access_token\s*=\s*'.*'", "access_token = ''", content)
+# ذخیره فایل با تغییرات
+with open('uploader_copy.py', 'w') as file:
+    file.write(content)
+
+
+print("Connecting To Github...")
+
+g = Github(access_token)
+repo = g.get_user().get_repo(repository_name)
+
+
+file_path = "./get.py"
+
+with open(file_path, 'r') as file:
+    content = file.read()
+
+now = datetime.now()
+current_time = now.strftime("%H:%M")
+timee=str(current_time)
+
+file_name = "app/get.py"
+file_contents = repo.get_contents(file_name)
+sha = file_contents.sha
+
+commit_message = f"Update at {timee}"
+print(f"Uploading {file_path} as {file_name}")
+repo.update_file(file_name, commit_message, content, sha)
+
+print(f"{file_path} Uploaded.")
+print(f"--------------------")
+
+file_path = "./uploader_copy.py"
+
+with open(file_path, 'r') as file:
+    content = file.read()
+
+now = datetime.now()
+current_time = now.strftime("%H:%M")
+timee=str(current_time)
+
+file_name = "app/uploader.py"
+file_contents = repo.get_contents(file_name)
+sha = file_contents.sha
+
+commit_message = f"Update at {timee}"
+print(f"Uploading {file_path} as {file_name}")
+repo.update_file(file_name, commit_message, content, sha)
+
+print(f"{file_path} Uploaded.")
+print(f"--------------------")
+
+
+print("Upload successfully!")
+
+
+
+# پاک کردن فایل کپی شده
+print("Deleting Temp File...")
+os.remove('uploader_copy.py')
+print("Done ._.")
